03cb31c029fd4728146500a7d43a3d5323bb5e0b
AlexeyRyabov <ryabovalexey04@gmail.com>
2022-12-10 09:18:07 +0300

    fix i18n

--- src/i18n.js
+++ src/i18n.js
@@ -2,9 +2,15 @@ import i18n from 'i18next'
 import Backend from 'i18next-http-backend'
 import LanguageDetector from 'i18next-browser-languagedetector'
 import { initReactI18next} from 'react-i18next'
+import translationEN from "./locales/en/translation.json"
+import translationRU from "./locales/ru/translation.json"
+const resources = {
+    en: {translation: translationEN},
+    ru: {translation: translationRU},
 
+}
 i18n.use(Backend).use(LanguageDetector).use(initReactI18next).init({
-    fallbackLng: 'ru',
+    fallbackLng: "ru",
     debug: true,
     detection: {
         order: ['localStorage', 'cookie'],
